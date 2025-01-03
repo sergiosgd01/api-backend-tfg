17adcf3cf2c2a70240555f437af578fbfd24311c
Sergio <sergiosgd2001@gmail.com>
2025-01-03 10:31:00 +0100

    quitado getEventByCode

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { getEvents, getEventsOrganization, getEventByCode, getEventById, changeStatusEvent, getEventQRCode, createEvent, editEvent, deleteEvent } = require('../controllers/eventController');
+const { getEvents, getEventsOrganization, getEventById, changeStatusEvent, getEventQRCode, createEvent, editEvent, deleteEvent } = require('../controllers/eventController');
 const router = express.Router();
 
 router.get('/', getEvents); 
-router.get('/getEventById/:id', getEventById); 
+router.get('/:id', getEventById); 
 router.get('/getEventQRCode', getEventQRCode); 
 router.get('/getEventsOrganization/:organizationCode', getEventsOrganization); 
-router.get('/:code', getEventByCode); 
 
 router.put('/cancel/:id', changeStatusEvent);
 router.put('/editEvent/:id', editEvent);
